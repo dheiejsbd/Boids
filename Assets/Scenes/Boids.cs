3bade14309000473158a4e4997b17b8c0520d45b
dheiejsbd <sulwoo01@gmail.com>
2022-05-10 00:31:23 +0900

    Boids -Separation -Alignment -Cohesion
    -Bounds
    -Obstacle

---
+++ Assets/Scenes/Boids.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Boids : MonoBehaviour
+{
+    [SerializeField] GameObject obj;
+    [SerializeField] int Count;
+    public bool Space3D;
+    [SerializeField] float SpawnRange;
+    public float MaxDist = 100;
+
+    public Vector2 speed;
+    public float RotateSpeed = 1f;
+    public float fov;
+    public float NeighbourRadius = 10;
+    public int maxNeighbourCount = 50;
+    public float obstacleDist;
+    public LayerMask obstacleLayer;
+
+    [Header("피하는 방향")]
+    [Range(0,10)] public float SeparationWight = 1;
+    [Header("평균 방향")]
+    [Range(0,10)] public float AlignmentWight = 1;
+    [Header("중심 방향")]
+    [Range(0,10)] public float CohesionWight = 1;
+    [Range(0,10)]public float BoundsWight = 1;
+    [Range(0,10)] public float obstacleWight = 10;
+
+    private void Start()
+    {
+
+        for (int i = 0; i < Count; i++)
+        {
+            GameObject instance = Instantiate(obj, transform);
+            instance.GetComponent<BoidsUnit>().OnInitialized(this);
+
+            if(Space3D)
+            {
+                instance.transform.position += Random.insideUnitSphere * SpawnRange;
+                instance.transform.rotation = Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+            }
+            else
+            {
+                var v = Random.insideUnitCircle * SpawnRange;
+                instance.transform.position += new Vector3(v.x, 0, v.y);
+                instance.transform.rotation = Quaternion.Euler(new Vector3(0,Random.Range(0, 360),0));
+            }
+        
+        }
+    }
+
+}
---
+++ Assets/Scenes/BoidsUnit.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class BoidsUnit : MonoBehaviour
+{
+    Boids Boids;
+    float speed;
+    Vector3 RandomVec;
+    public void OnInitialized(Boids _boids)
+    {
+        Boids = _boids;
+        speed = Random.Range(Boids.speed.x, Boids.speed.y);
+        StartCoroutine(RandomVecCor());
+    }
+    void Update()
+    {
+        Transform[] targets = Change(Physics.OverlapSphere(transform.position, Boids.NeighbourRadius), (Collider coll) => coll.transform);
+
+        if(targets.Length > Boids.maxNeighbourCount)
+        {
+            var t = new List<Transform>();
+            int count = 0;
+            for (int i = 0; i < targets.Length; i++)
+            {
+                if(Vector3.Angle(transform.forward, targets[i].position - transform.position) <= Boids.fov)
+                {
+                    count++;
+                    t.Add(targets[i]);
+                    if (count == Boids.maxNeighbourCount) break;
+                }
+            }
+            targets = t.ToArray();
+        }
+        Debug.DrawRay(transform.position, Separation(targets).normalized, Color.red);
+        Debug.DrawRay(transform.position, Alignment(targets).normalized, Color.blue);
+        Debug.DrawRay(transform.position, Cohesion(targets).normalized, Color.green);
+
+        var SeparationVec = Separation(targets).normalized * Boids.SeparationWight;
+        var  AlignmentVec = Alignment(targets).normalized * Boids.AlignmentWight;
+        var   CohesionVec = Cohesion(targets).normalized * Boids.CohesionWight;
+        
+        var boundsVec = Bounds() * Boids.BoundsWight;
+        var obstacle = Obstacle().normalized * Boids.obstacleWight;
+
+        var LookVec = SeparationVec + AlignmentVec + CohesionVec + boundsVec + obstacle + RandomVec ;
+
+        if (!Boids.Space3D) LookVec.y = 0;
+
+        LookVec = Vector3.Lerp(transform.forward, LookVec, Time.deltaTime * Boids.RotateSpeed);
+        Quaternion targetQuaternion = Quaternion.LookRotation(LookVec);
+        transform.rotation = targetQuaternion;
+    }
+
+    IEnumerator RandomVecCor()
+    {
+        while (true)
+        {
+            speed = Random.Range(Boids.speed.x, Boids.speed.y);
+            if(Boids.Space3D)
+            {
+                RandomVec = Random.insideUnitSphere;
+            }
+            else
+            {
+                var v = Random.insideUnitCircle;
+                RandomVec = new Vector3(v.x, 0, v.y);
+            }
+            yield return new WaitForSeconds(Random.Range(0f, 3f));
+        }
+    }
+    private void LateUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    //무리를 피하는 방향
+    Vector3 Separation(Transform[] _targets)
+    {
+        Vector3[] targets = Change(_targets, (Transform target) => transform.position - target.position);
+        return Average(targets);
+    }
+    //무리의 평균 이동방향
+    Vector3 Alignment(Transform[] _targets)
+    {
+        Vector3[] targets = Change(_targets, (Transform target) => target.forward);
+        return Average(targets);
+    }
+    //무리의 중심 방향
+    Vector3 Cohesion(Transform[] _targets)
+    {
+        Vector3[] targets = Change(_targets, (Transform target) => target.position);
+        return Average(targets) - transform.position;
+    }
+
+    //최대 거리
+    Vector3 Bounds()
+    {
+        var dist = Boids.transform.position - transform.position;
+        return dist / Boids.MaxDist;
+    }
+
+    // 장애물
+    Vector3 Obstacle()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Boids.obstacleDist, Boids.obstacleLayer)) return hit.normal;
+        return Vector3.zero;
+    }
+
+    Result[] Change<Target, Result>(Target[] target, Func<Target, Result> func)
+    {
+        Result[] results = new Result[target.Length];
+        for (int i = 0; i < target.Length; i++)
+        {
+            results[i] = func(target[i]);
+        }
+        return results;
+    }
+
+    Vector3 Add(Vector3[] targets)
+    {
+        Vector3 delta = Vector3.zero;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            delta += targets[i];
+        }
+        return delta;
+    }
+    Vector3 Add(Vector3[] targets, Func<Vector3, Vector3> func)
+    {
+        Vector3 delta = Vector3.zero;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            delta += func(targets[i]);
+        }
+        return delta;
+    }
+
+    Vector3 Average(Vector3[] targets)
+    {
+        return Add(targets) / targets.Length;
+    }
+    Vector3 Average(Vector3[] targets, Func<Vector3, Vector3> func)
+    {
+        return Add(targets, func) / targets.Length;
+    }
+}
